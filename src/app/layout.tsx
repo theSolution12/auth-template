913e78a9a14beed7c15af29900a3007fb689d337
Parth Patidar <parth1207patidar@gmail.com>
2025-07-10 14:37:08 +0530

    feat(query): Implement React Query provider and integrate into layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "react-hot-toast";
+import { QueryProvider } from "@/lib/react-query/query-provider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,7 +29,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <QueryProvider>{children}</QueryProvider>
         <Toaster position="top-right" toastOptions={{
         duration: 3000,
         style: {
